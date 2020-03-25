94884b57b8cb69450fb9edc1e8a6d81240c568df
AutomeEdwin <automeedwin@gmail.com>
2020-03-25 11:54:16 +0100

    Réorganisation du repo pour facilité l'utilisation de docker-compose

---
+++ Patrick/Intranet/index.php
@@ -0,0 +1,7 @@
+-----------------
+<p>Bienvenue sur l'<b><span style="color:red">intranet !</span></b></p>
+<?php 
+echo '----------PHP----------'."<br><br>";
+echo 'Version courante de PHP : '.'<b><span style="color:blue">' . Phpversion().'</b></span><br><br>';
+echo '----------PHP----------';
+ ?>
