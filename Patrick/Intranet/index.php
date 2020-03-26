bb76ba407e0bad445dd6ff953630a52b86a79ebe
AutomeEdwin <automeedwin@gmail.com>
2020-03-26 11:03:33 +0100

    commit

--- Patrick/Intranet/index.php
+++ Patrick/Intranet/index.php
@@ -1,7 +1,4 @@
------------------
 <p>Bienvenue sur l'<b><span style="color:red">intranet !</span></b></p>
-<?php 
-echo '----------PHP----------'."<br><br>";
-echo 'Version courante de PHP : '.'<b><span style="color:blue">' . Phpversion().'</b></span><br><br>';
-echo '----------PHP----------';
- ?>
+<?php
+echo 'coucou de php';
+?>
