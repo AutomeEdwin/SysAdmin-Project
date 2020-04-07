70bdd361679a478f85baa4ae649f394126094658
AutomeEdwin <automeedwin@gmail.com>
2020-04-07 16:11:58 +0200

    Update server web dmz

--- Edwin/Sites_DMZ/www/index.php
+++ Edwin/Sites_DMZ/www/index.php
@@ -1,5 +1,5 @@
 -----------------
-<p>Bienvenue sur l'<b><span style="color:red">intranet !</span></b></p>
+<p>Bienvenue sur le site <b>B2B de WoodyToys</b></p>
 <?php 
 echo '----------PHP----------'."<br><br>";
 echo 'Version courante de PHP : '.'<b><span style="color:blue">' . Phpversion().'</b></span><br><br>';
