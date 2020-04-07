de4b0f106d5494e5028f7f5ba0c0b3b2e20ab934
AutomeEdwin <automeedwin@gmail.com>
2020-04-07 15:31:29 +0200

    rm

--- Edwin/B2B/index.php
+++
@@ -1,7 +0,0 @@
------------------
-<p>Bienvenue sur le site B2B</p>
-<?php 
-echo '----------PHP----------'."<br><br>";
-echo 'Version courante de PHP : '.'<b><span style="color:blue">' . Phpversion().'</b></span><br><br>';
-echo '----------PHP----------';
- ?>
