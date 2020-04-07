f697d39801c1869d08b3275acfe29c11c25528ba
AutomeEdwin <automeedwin@gmail.com>
2020-04-07 14:00:13 +0200

    Rework serveur apache 1

--- Edwin/Sites_DMZ/www/index.php
+++ Edwin/Sites_DMZ/www/index.php
@@ -1,5 +1,5 @@
 -----------------
-<p>Bienvenue sur le site B2B</p>
+<p>Bienvenue sur l'<b><span style="color:red">intranet !</span></b></p>
 <?php 
 echo '----------PHP----------'."<br><br>";
 echo 'Version courante de PHP : '.'<b><span style="color:blue">' . Phpversion().'</b></span><br><br>';
