23e950ba18c4165dbb9feb6c432d9cbc950747e6
AutomeEdwin <automeedwin@gmail.com>
2020-05-07 16:35:27 +0200

    update b2b

--- Edwin/Sites_DMZ/www/b2b/index.php
+++ Edwin/Sites_DMZ/www/b2b/index.php
@@ -1,6 +1,6 @@
 <?php
-    try{
-        $db = new PDO('mysql:host=localhost; dbname=jouets; charset=utf8', 'admin', 'sqlpasswd');
+     try{
+        $db = new PDO('mysql:host=51.178.40.8; port=4200; dbname=wtDB; charset=utf8', 'dbAdmin', '');
     } catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
     }
@@ -8,18 +8,17 @@
 
 -----------------
 <p>Bienvenue sur le site <b>B2B de WoodyToys</b></p>
-<?php 
+
+<?php
     $reponse = $db->query('SELECT * FROM jouets');
-    
     while($data = $reponse->fetch()){
 ?>
 
 <p>
-<strong>Jouet : </strong> <?php echo $data['jouetNom']; ?><br> 
+<strong>Jouet : </strong> <?php echo $data['jouetNom']; ?><br>
 <strong>Prix : </strong> <?php echo $data['jouetPrix']; ?><br>
-</p>    
+</p>
 
 <?php
     };
-    $reponse->closeCursor();
  ?>
