ccc5eb1230bfdf4f0ad32ffef8d51fafd0e917c2
AutomeEdwin <automeedwin@gmail.com>
2020-05-13 15:18:55 +0200

    update code php

--- Edwin/Sites_DMZ/www/b2b/index.php
+++ Edwin/Sites_DMZ/www/b2b/index.php
@@ -1,18 +1,6 @@
 <?php
-<<<<<<< HEAD
      $db = mysqli_connect('51.178.41.110:4200', 'dbAdmin', 'sqlpasswd', 'wtDB');
-=======
-    try{
-        $db = new PDO('mysql:host=admin; dbname=wtDB; charset=utf8', 'admin', 'sqlpasswd');
-    } catch (Exception $e){
-        die('Erreur : ' . $e->getMessage());
-    }
->>>>>>> a869b2af60b451cd54f1c6e10c9d3387c718b685
 ?>
-<html>
-<head>
-</head>
-<body>
 -----------------
 <p>Bienvenue sur le site <b>B2B de WoodyToys</b></p>
 
